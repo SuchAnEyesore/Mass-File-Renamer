9d42efb74b265fac89a1bbc9f1f8425f15d4e020
eyesore <66234516+SuchAnEyesore@users.noreply.github.com>
2020-10-22 04:36:44 -0500

    Update Mass File Renamer.py

--- Mass File Renamer.py
+++ Mass File Renamer.py
@@ -1,27 +1,26 @@
+
 import os
 import sys
 os.system("mode con cols=80 lines=20")
 def fileRenamer():
-    print(" Mass File Renamer ".center(80, '#'))
-folderDir = input("Folder Directory:")
-fileName = input("New File Name:")
-os.chdir(folderDir)
-i = 1
-for file in os.listdir():
-    src = file
-oldext = os.path.splitext(file)[1]
-dst = fileName + str(i) + oldext
-os.rename(src, dst)
-i += 1
-answer = input("\nFinished! \nWant To Rename Other Files? (y/n):")
-if answer in ['y', 'Y', 'yes', 'Yes', 'YES']:
-    os.system('cls'
-        if os.name == 'nt'
-        else 'clear')
-fileRenamer()
-if answer in ['n', 'N', 'no', 'No', 'NO']:
-    exit()
+ print(" Mass File Renamer ".center(80, '#'))
+ folderDir = input("Folder Directory: ")
+ fileName = input("New File Name: ")
+ os.chdir(folderDir)
+ i=1
+ for file in os.listdir():
+      src=file
+      oldext = os.path.splitext(file)[1]
+      dst=fileName+str(i)+oldext
+      os.rename(src,dst)
+      i+=1
+ answer = input("\nFinished! \nWant To Rename Other Files? (y/n):")
+ if answer in ['y', 'Y', 'yes', 'Yes', 'YES']:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    fileRenamer()
+ if answer in ['n', 'N', 'no', 'No', 'NO']:
+     exit()
+   
 
-
-
+   
 fileRenamer()
